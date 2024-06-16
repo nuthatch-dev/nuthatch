f4e8ed8c97c014667d874bb8d8650e03eb3e00c0
Eugene Elantsev <blinktreeman@gmail.com>
2024-06-16 23:27:34 +0700

    Init Building Object service

---
+++ building-object/src/test/java/ru/nuthatch/buildingobject/BuildingObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nuthatch.buildingobject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingObjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
