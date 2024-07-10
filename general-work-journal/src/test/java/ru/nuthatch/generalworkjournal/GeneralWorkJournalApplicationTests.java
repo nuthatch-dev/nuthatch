015aa3d8acf4448269405facb47ae68bbfc89900
Eugene Elantsev <blinktreeman@gmail.com>
2024-07-10 23:03:19 +0700

    Initial commit

---
+++ general-work-journal/src/test/java/ru/nuthatch/generalworkjournal/GeneralWorkJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nuthatch.generalworkjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralWorkJournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
