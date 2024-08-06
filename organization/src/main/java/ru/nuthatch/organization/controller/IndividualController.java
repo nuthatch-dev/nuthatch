a81b380e2056e231d5c4bbe0454b1eb22dd99ed6
Eugene Elantsev <blinktreeman@gmail.com>
2024-08-06 14:09:22 +0700

    Add controllers, remove passport details service

---
+++ organization/src/main/java/ru/nuthatch/organization/controller/IndividualController.java
@@ -0,0 +1,56 @@
+package ru.nuthatch.organization.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.nuthatch.organization.entity.Individual;
+import ru.nuthatch.organization.service.IndividualService;
+
+import java.util.Collection;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/api/v1/individual")
+public class IndividualController {
+
+    private final IndividualService service;
+
+    @Autowired
+    public IndividualController(IndividualService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Individual> save(@RequestBody Individual entity) {
+        return new ResponseEntity<>(service.save(entity), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Individual> findById(@RequestParam(name = "id") UUID uuid) {
+        return service
+                .findById(uuid)
+                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping(value = "/all")
+    public ResponseEntity<Collection<Individual>> findAll() {
+        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Individual> update(@RequestBody Individual entity) {
+        return service
+                .update(entity)
+                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<HttpStatus> deleteById(@RequestParam(name = "id") UUID uuid) {
+        service.deleteById(uuid);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
---
+++ organization/src/main/java/ru/nuthatch/organization/controller/IndividualEntrepreneurController.java
@@ -0,0 +1,56 @@
+package ru.nuthatch.organization.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.nuthatch.organization.entity.IndividualEntrepreneur;
+import ru.nuthatch.organization.service.IndividualEntrepreneurService;
+
+import java.util.Collection;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/api/v1/individual-entrepreneur")
+public class IndividualEntrepreneurController {
+
+    private final IndividualEntrepreneurService service;
+
+    @Autowired
+    public IndividualEntrepreneurController(IndividualEntrepreneurService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<IndividualEntrepreneur> save(@RequestBody IndividualEntrepreneur entity) {
+        return new ResponseEntity<>(service.save(entity), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<IndividualEntrepreneur> findById(@RequestParam(name = "id") UUID uuid) {
+        return service
+                .findById(uuid)
+                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping(value = "/all")
+    public ResponseEntity<Collection<IndividualEntrepreneur>> findAll() {
+        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<IndividualEntrepreneur> update(@RequestBody IndividualEntrepreneur entity) {
+        return service
+                .update(entity)
+                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<HttpStatus> deleteById(@RequestParam(name = "id") UUID uuid) {
+        service.deleteById(uuid);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
